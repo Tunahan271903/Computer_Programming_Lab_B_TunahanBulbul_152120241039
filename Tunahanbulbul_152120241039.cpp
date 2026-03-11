8d08fee3e134ad642d4f7d92ff5ae7b7faf219f5
Tunahan271903 <tbulbul046@gmail.com>
2026-03-11 16:25:42 +0300

    Add files via upload

---
+++ Tunahanbulbul_152120241039.cpp
@@ -0,0 +1,6 @@
+﻿Build started at 3:54 PM...
+1>------ Build started: Project: Project27, Configuration: Debug x64 ------
+1>15210241039_Tunahanhilmibulbul.cpp
+1>Project27.vcxproj -> C:\Users\neuer\source\repos\Project27\x64\Debug\Project27.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+========== Build completed at 3:54 PM and took 00.789 seconds ==========
